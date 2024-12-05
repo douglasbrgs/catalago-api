c23d402fb32214812043ee4992242d35e0752a09
Douglas Borges <douglasborges19@gmail.com>
2024-12-05 20:54:04 -0300

    ProdutosController - POST

--- CatalogoApi/Controllers/ProdutosController.cs
+++ CatalogoApi/Controllers/ProdutosController.cs
@@ -28,7 +28,7 @@ namespace CatalogoApi.Controllers
             return produtos;
         }
 
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}", Name = "ObterProduto")]
         public ActionResult<Produto> Get(int id)
         {
             var produto = _context.Produtos.FirstOrDefault(x => x.ProdutoId == id);
@@ -39,6 +39,20 @@ namespace CatalogoApi.Controllers
             }
 
             return produto;
+        }
+
+        [HttpPost]
+        public ActionResult Post(Produto produto)
+        {
+            if (produto is null)
+            {
+                return BadRequest();
+            }
+
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId }, produto);
         }
     }
 }
