e53346db4c5fbae43b42beafb5550cbf59037b7a
Douglas Borges <douglasborges19@gmail.com>
2024-12-10 07:16:43 -0300

    Validação Personalizada 1

--- CatalogoApi/Models/Produto.cs
+++ CatalogoApi/Models/Produto.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using CatalogoApi.Validations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
@@ -12,6 +13,7 @@ public class Produto
 
     [Required(ErrorMessage = "O nome é obrigatório")]
     [StringLength(80, ErrorMessage = "O nome deve ter no máximo {1} e no mínimo {2} caracteres", MinimumLength = 5)]
+    [PrimeiraLetraMaiuscula]
     public string? Nome { get; set; }
 
     [Required]
---
+++ CatalogoApi/Validations/PrimeiraLetraMaiusculaAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CatalogoApi.Validations;
+
+public class PrimeiraLetraMaiusculaAttribute : ValidationAttribute
+{
+    protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+    {
+        var primeiraLetra = value?.ToString()[0].ToString();
+
+        if (primeiraLetra != primeiraLetra.ToUpper())
+        {
+            return new ValidationResult("A primeira letra do nome deve ser maiúscula");
+        }
+
+        return ValidationResult.Success;
+    }
+}
