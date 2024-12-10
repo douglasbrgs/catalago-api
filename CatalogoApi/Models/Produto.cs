e234bb59a8d92afd10a97d16c7d20952f4d70602
Douglas Borges <douglasborges19@gmail.com>
2024-12-10 07:41:25 -0300

    Validação Personalizada 2

--- CatalogoApi/Models/Produto.cs
+++ CatalogoApi/Models/Produto.cs
@@ -6,14 +6,14 @@ using System.Text.Json.Serialization;
 namespace CatalogoApi.Models;
 
 [Table("Produtos")]
-public class Produto
+public class Produto : IValidatableObject
 {
     [Key]
     public int ProdutoId { get; set; }
 
     [Required(ErrorMessage = "O nome é obrigatório")]
     [StringLength(80, ErrorMessage = "O nome deve ter no máximo {1} e no mínimo {2} caracteres", MinimumLength = 5)]
-    [PrimeiraLetraMaiuscula]
+    //[PrimeiraLetraMaiuscula]
     public string? Nome { get; set; }
 
     [Required]
@@ -35,4 +35,24 @@ public class Produto
 
     [JsonIgnore]
     public Categoria? Categoria { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (!String.IsNullOrEmpty(this.Nome))
+        {
+            var primeiraLetra = this.Nome[0].ToString();
+
+            if (primeiraLetra != primeiraLetra.ToUpper())
+            {
+                yield return new ValidationResult("A primeira letra do nome do produto deve ser maiúscula",
+                    new[] { nameof(this.Nome) });
+            }
+        }
+
+        if (this.Estoque <= 0)
+        {
+            yield return new ValidationResult("O estoque deve ser maior que zero",
+                new[] { nameof(this.Estoque) });
+        }
+    }
 }
